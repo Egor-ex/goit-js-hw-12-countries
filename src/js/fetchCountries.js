36adc1ed3b7ee0699cc484d35c8be81950d5c4aa
Egor Plyatsko <mr.error406@gmail.com>
2021-09-26 21:42:39 +0300

    fix

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 function fetchCountries(searchQuery) {
-    const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
-    const SEARCH_PARAMS = 'fields=name;capital;population;flag;languages';
+    const BASE_URL = 'https://restcountries.com/v3/name/';
+    const SEARCH_PARAMS = 'name;capital;area;flag;languages';
     return fetch(`${BASE_URL}${searchQuery}?${SEARCH_PARAMS}`).then(res =>
         res.json(),
     );
